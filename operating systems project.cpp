cf48ae154a3a83b7cd8264e32ac49c1dc1ec1b23
oghenemanoomogha <76177578+oghenemanoomogha@users.noreply.github.com>
2020-12-17 22:44:36 +0200

    Update operating systems project.cpp

--- operating systems project.cpp
+++ operating systems project.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #define SIZE 10
 using namespace std;
@@ -6,13 +5,14 @@ struct Data{
 	int BurstTime;
 	int ArrivalTime;
 	int Priority;
-}c[SIZE];
+};
 
 
 int enterTIME();
 int SwitchN();
 int SwitchSch();
 int calcFOFO();
+void swap(int, int);
 
 int main(){
 	struct Data a;
@@ -22,6 +22,13 @@ int main(){
 		
 		
 
+}
+void swap(int *y, int *z){
+	//using swapping to find to sort the minimmum
+	int temp;
+	temp = *y;
+	*y = *z;
+	*z = temp;
 }
 
 int SwitchSch(){
@@ -38,6 +45,7 @@ int SwitchSch(){
 
 int SwitchN(){
 	int a;
+	struct Data c[SIZE];
 	cout<<"--------->CPU Schedular Simulator<--------->"<<endl;
 	cout<<"1) Scheduling Method"<<endl;
 	cout<<"2) preemptive Mode"<<endl;
@@ -46,17 +54,19 @@ int SwitchN(){
 	cout<<"5) End Program"<<endl;
 	cout<<"Option"<<endl;
 	cin>>a;
-	switch(a){
-		
-		case 1:
-			//added the schedule switch
+	if(a == 1){
 			SwitchSch();
 		 int decision;
 			
 			cin>>decision;
-			switch(decision){
-				case 1: 
-					int n;int waitingtime ;int waiting[n] ;float avg; int sum;
+			if(decision==1){
+			
+			
+					int n;
+					int waitingtime ;
+					int waiting[n] ;
+					float avg;
+					int sum;
 				
 		 
 	                	cout<<"please enter number of process"<<endl;
@@ -70,7 +80,9 @@ int SwitchN(){
 		            	
 		             	
 		            }
-		            cout<<"\n process\t\Burst Time\twaiting Time";
+		            cout<<"============================Scheduling chart==============================="<<endl;
+
+		            cout<<"\n process\tBurst Time\twaiting Time";
 		            
                       	for(int i = 0; i < n; i++){
 		              		waiting[i] = c[i-1].BurstTime+ waiting[i-1];
@@ -88,28 +100,23 @@ int SwitchN(){
 		              	}
 		              	avg = sum/n;
 		              	cout<<"\naverage of waiting time"<<avg;
-		              	break;
+		              	cout<<endl;
 		              	
 		              	
-	            
-	        
-	            
-	                 
-	
-			
-			
-	
-		}
+		             
+		              	
+						}
+						
+						if(decision == 2){
+							int p;
+							cout<<"enter number of process"<<endl;
+							cin>>p;
+							
+						}
+					}
+					
+					
+		             
 		
 	}
-	
-	
-	
-	
-	
-}
-
-
-	
-	
 
