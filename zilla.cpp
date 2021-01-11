ccbe4abe5939494cd425044607546924ac82b41b
oghenemano omogha <76177578+omoghaoghenemano@users.noreply.github.com>
2021-01-11 23:10:58 +0200

    Fcfs ,sjf, rr, premptive,

--- zilla.cpp
+++ zilla.cpp
@@ -3,6 +3,8 @@
 #include<algorithm>
 #include<vector>
 #include<deque>
+//sir please permit me to use deque library and vector
+#include<fstream>
 #define LINE_MAX 30
 
 using namespace std;
@@ -13,11 +15,11 @@ struct node{
            burst,
            priority,
            waiting,
-           turnaroundtime;
-          
+           turnaroundtime,
+           processno;
            
   
-       node() : id(0), arrive(0), burst(0), priority(0), waiting(0), turnaroundtime(0){};
+       node() : id(0), arrive(0), burst(0), priority(0), waiting(0), turnaroundtime(0), processno(0){};
 };
 
 class CPU{
@@ -26,19 +28,24 @@ class CPU{
    public:
        CPU(){};
        void swappingval(deque<node>::iterator num1, deque<node>::iterator num2);
-       void display_original();
-       void Display();
        void FirstComeFirstServe(int process);
        void shortestjobfirstNP(int process);
        void Enterval(node data);
        void PriorityNonPremptive(int process);
        void RoundRobin(int quantum, int process);
+       void shortestjobfirstpremptive(int process);
+       void prioritypremptive(int process);
+      
+       void displayOutput();
+       
       
          
 	private:
        deque<node> Processid;
        deque<node> Processchange;
+       deque<node> make;
        vector<node>completed;
+       vector<node>compared;
        deque<node> burstProcess;
       
 };
@@ -65,6 +72,10 @@ int main(){
            show.Enterval(data);
 }
 fclose(fp);
+ if( remove( "output.txt" ) != 0 )
+    perror( "Error deleting file" );
+  else
+    puts( "Output file cleared automatically" );
  int choice;
     while(1){
     	
@@ -81,9 +92,9 @@ fclose(fp);
     	switch(choice){
     		case 1:
     			cout<<"1) First come, First Served Scheduling;"<<endl;
-    			cout<<"2) Shortest-Job-First Scheduling "<<endl;
-    			cout<<"3) Priority Scheduling"<<endl;
-    			cout<<"4) Round-Robin Scheduling"<<endl;
+    			cout<<"2) Shortest-Job-First Scheduling:"<<endl;
+    			cout<<"3) Priority Scheduling:"<<endl;
+    			cout<<"4) Round-Robin Scheduling:"<<endl;
     			cout<<"option"<<endl;
     			 int option;
     			 cin>>option;
@@ -106,7 +117,22 @@ fclose(fp);
     		
     			break;
     		case 2: 
-			 cout<<"non premeptive"<<endl;	
+    		int prem;
+    		cout<<"premptive shedule"<<endl;
+		    cout<<"1) ShortJOB premeptive"<<endl;
+		    cout<<"2) Priority premeptive"<<endl;
+		 cin>>prem;
+		    if(prem==1){
+		    	cout<<"shortjob premeptive"<<endl;
+		    	show.shortestjobfirstpremptive(process);
+			}
+			if(prem==2){
+				cout<<"Output is Written in a File"<<endl;
+				show.prioritypremptive(process);
+				
+			}
+			     
+				 	
 			     break;  
 			 
 			 case 3:
@@ -120,11 +146,12 @@ fclose(fp);
 			        break;
 			 	
 			 	
-			 	case 4: cout<<"show result"<<endl;
-			 	 show.display_original(); 
+			 	case 4: 
+			 	 show.displayOutput();
 			 	break;
 			 	
 			 	case 5:
+			 		show.displayOutput();
 			 		exit(1);
 		}
     	
@@ -136,27 +163,29 @@ fclose(fp);
    
    return 0;
 }
-void CPU::Display(){
-  
-   cout << "Process Waiting Times:" <<endl;
-  
-   for (int i = 0; i < completed.size(); i++){
-       cout << "P" << completed[i].id <<":" << " "<< completed[i].waiting << endl;
-   }
-   cout << endl;
+void CPU::displayOutput(){
+	ifstream file;
+	char g[1000];
+	
+	file.open("output.txt");
+	if(!file){
+		cout<<"error opening the file"<<endl;
+		exit(0);
+	}
+	cout<<endl;
+	while(file.eof()==0){
+			
+		file>>g;
+	
+		cout<<g<<" "<<endl;
+	}
+	cout<<"\n";
+	file.close();
 }
 
 
 
-void CPU::display_original(){
-  
-   cout  << "Process" << endl;
-  
-   for (int i = 0; i < Processid.size(); i++){
-       cout  <<"P" << Processid[i].id <<" "<<Processid[i].arrive <<" "<< Processid[i].burst << " "<< Processid[i].priority << endl;
-   }
-   cout << endl;
-}
+
 
 void CPU::swappingval(deque<node>::iterator num1, deque<node>::iterator num2){
    deque<node>::iterator temp = num1;
@@ -168,7 +197,9 @@ void CPU::swappingval(deque<node>::iterator num1, deque<node>::iterator num2){
 bool sortArrival(const node a, const node b){
    return a.arrive < b.arrive;
 }
-
+bool sortProcess(const node a, const node b){
+	return a.id < b.id;
+}
 void CPU::Enterval(node data){
   
    Processid.push_back(data);
@@ -193,12 +224,23 @@ void CPU::FirstComeFirstServe(int process){
    for (num = Processid.begin(); num != Processid.end(); num++){
        time += num->burst;
        num->waiting = time - num->arrive - num->burst;
-       num->turnaroundtime = time - num->arrive;
+      num->turnaroundtime = time - num->arrive;
        waitingtime += num->waiting;
        
        completed.push_back((*num));
    }
-       cout << " First Come First Served: " << endl;
+       ofstream myfile;
+       if(!myfile){
+       	myfile.open("output.txt");
+       	
+	   }
+	   else {
+	   
+	     myfile.open("output.txt",ios::app);
+	 }
+       
+       myfile<<"------------------------"<<endl;
+       myfile << " First Come First Served: " << endl;
        
        average = (float)waitingtime/process;
    
@@ -206,10 +248,10 @@ void CPU::FirstComeFirstServe(int process){
   
   
       for (int i = 0; i < completed.size(); i++){
-       cout << "P" << completed[i].id <<":" << " "<< completed[i].waiting << endl;
+       myfile << "P" << completed[i].id <<":" << completed[i].waiting << endl;
    }
-    cout<<"Average waiting time:"<<average;
-   cout << endl;
+    myfile<<"Average waiting time:"<<average;
+    myfile<< endl;
    
       
       
@@ -219,6 +261,7 @@ void CPU::FirstComeFirstServe(int process){
        }
       
        completed.clear();
+       myfile.close();
 }
 
 
@@ -239,8 +282,8 @@ void CPU::RoundRobin(int quantum, int process){
        if (num->burst <= quantum){
            time += num->burst;
            num->waiting += time - num->arrive - burstmain[num->id];
-           num->turnaroundtime += time - num->arrive;
-           waitingtime+=num->waiting;
+          num->turnaroundtime += time - num->arrive;
+          waitingtime+=num->waiting;
            completed.push_back((*num));
        }
        
@@ -250,18 +293,28 @@ void CPU::RoundRobin(int quantum, int process){
            Processchange.push_back((*num));
        }
    }
-   cout << "Round Robin Scheduling–time_quantum=2 : " << endl;
+    ofstream myfile2;
+       if(!myfile2){
+       	myfile2.open("output.txt");
+       	
+	   }
+	   else {
+	   
+	     myfile2.open("output.txt",ios::app);
+	 }
+        myfile2<<"------------------------"<<endl;
+   myfile2<< "RoundRobinScheduling–time_quantum=2:" << endl;
     average = (float)waitingtime/process;
    
    
 
   
       for (int i = 0; i < completed.size(); i++){
-       cout << "P" << completed[i].id <<":" << " "<< completed[i].waiting << endl;
+       myfile2<< "P" << completed[i].id <<":" << completed[i].waiting << endl;
    }
-    cout<<"Average waiting time:"<<average;
-   cout << endl;
-   
+    myfile2<<"Average waiting time:"<<average;
+   myfile2<< endl;
+   myfile2.close();
   
    
 }
@@ -300,12 +353,12 @@ sort(Processid.begin(), Processid.end(), sortArrival);
            }
        }
       
-       sort (type.begin(), type.end(), sortPriority);
+        sort (type.begin(), type.end(), sortPriority);
        deque<node>::iterator end = type.begin();
       
        time += end->burst;
-       end->waiting += time - end->burst - end->arrive;
-       end->turnaroundtime += time - end->arrive;
+         end->waiting += time - end->burst - end->arrive;
+      end->turnaroundtime += time - end->arrive;
        waitingtime+= end->waiting;
       
        completed.push_back((*end));
@@ -315,16 +368,28 @@ sort(Processid.begin(), Processid.end(), sortArrival);
   
 
   
+   
+  ofstream myfile4;
+  
     average = (float)waitingtime/process;
-   
-   
-   cout << "Priority Scheduling Non-Preemptive: " << endl;
+       if(!myfile4){
+       	myfile4.open("output.txt");
+       	
+	   }
+	   else {
+	   
+	     myfile4.open("output.txt",ios::app);
+	 
+	 }
+	  myfile4<<"------------------------"<<endl;
+    myfile4<<"Priority Scheduling Non-Preemptive: " << endl;
   
       for (int i = 0; i < completed.size(); i++){
-       cout << "P" << completed[i].id <<":" << " "<< completed[i].waiting << endl;
+       myfile4<< "P" << completed[i].id <<":" << completed[i].waiting << endl;
    }
-    cout<<"Average waiting time:"<<average;
-   cout << endl;
+    myfile4<<"Average waiting time:"<<average;
+   myfile4<< endl;
+   myfile4.close();
    
   
    for (num = Processid.begin(); num != Processid.end(); num++){
@@ -337,7 +402,7 @@ sort(Processid.begin(), Processid.end(), sortArrival);
 void CPU::shortestjobfirstNP(int process){
 
 
-   sort(Processid.begin(), Processid.end(), sortArrival);
+  sort(Processid.begin(), Processid.end(), sortArrival);
   
    deque<node>::iterator num, start = Processid.begin();
   
@@ -347,8 +412,8 @@ void CPU::shortestjobfirstNP(int process){
     bool checked[10];
     float average = 0, waitingtime = 0;
    int time = start->burst;
-   start->waiting += time - start->burst - start->arrive;
-   start->turnaroundtime += time - start->arrive;
+    start->waiting += time - start->burst - start->arrive;
+     start->turnaroundtime += time - start->arrive;
   
    completed.push_back((*start));
   
@@ -375,31 +440,39 @@ void CPU::shortestjobfirstNP(int process){
            }
        }
        
-       sort (type.begin(), type.end(), sortBurst);
+        sort (type.begin(), type.end(), sortBurst);
        deque<node>::iterator end = type.begin();
       
        time += end-> burst;
-       end->waiting += time - end->burst - end->arrive;
+        end->waiting += time - end->burst - end->arrive;
        end->turnaroundtime += time - end->arrive;
-       waitingtime += end->waiting;
+        waitingtime += end->waiting;
         
        completed.push_back((*end));
       
        type.pop_front();
    }
+    average = waitingtime/process;
+   ofstream myfile1;
+       if(!myfile1){
+       	myfile1.open("output.txt");
+       	
+	   }
+	   else {
+	   
+	     myfile1.open("output.txt",ios::app);
+	 }
+       myfile1<<"------------------------"<<endl;
    
-   average = (float)waitingtime/process;
-   
-   
-   cout << "Shortest Job First NonPreemptive: " << endl;
+   myfile1<<"\n ShortestJobFirstNonPreemptive:" << endl;
   
       for (int i = 0; i < completed.size(); i++){
-       cout << "P" << completed[i].id <<":" << completed[i].waiting << endl;
+       myfile1<<"P" << completed[i].id <<":" << completed[i].waiting << endl;
    }
-    cout<<"Average waiting time:"<<average;
-   cout << endl;
+    myfile1<<"\n Average waiting time"<<average<<endl;
+   myfile1<<endl;
    
-  
+   myfile1.close();
    for (num = Processid.begin(); num != Processid.end(); num++){
            num->waiting = 0;
            num->turnaroundtime = 0;
@@ -410,6 +483,145 @@ void CPU::shortestjobfirstNP(int process){
    completed.clear();
 }
 
+void CPU::prioritypremptive(int process){
+	 int total = 0, timecalc = 0; int waitingtime = 0, average = 0;
+	int originalburst[10];
+	
+	for (deque<node>::iterator currentnum = Processid.begin(); currentnum != Processid.end(); currentnum++){
+		originalburst[currentnum->id] = currentnum->burst;
+	}
+	
+	sort(Processid.begin(), Processid.end(), sortArrival);
+	
+	 deque<node>::iterator num;
+	deque<node>::iterator currentnum;
+	deque<node>::iterator smallestnum;
+	for (num = Processid.begin(); num != Processid.end(); num++){
+		
+		deque<node>::iterator temp = num;
+		deque<node>::iterator next = ++temp; 
+		if (num == Processid.begin()){
+			timecalc += next->arrive - num->arrive; 
+			total += timecalc;
+			num->burst -= timecalc;
+		}
+		else if (next == Processid.end()){  
+			break;
+		}
+		else{
+			sort(burstProcess.begin(), burstProcess.end(), sortBurst);
+			
+			for (currentnum = burstProcess.begin(); currentnum != burstProcess.end(); currentnum++){
+				if (currentnum->burst == 0){
+					burstProcess.erase(currentnum);
+					continue;
+				}
+				else if (num->burst > currentnum->burst && currentnum->arrive <= total){
+					smallestnum = currentnum; 
+					break;
+			    }
+			    else{
+			    	smallestnum = num;
+				}
+		    }
+		    timecalc += 1;
+		 	timecalc += next->arrive - num->arrive; 
+		 	total += timecalc; 
+		 	smallestnum->burst -= timecalc; 
+		 	
+		 
+		 	if (smallestnum == num){
+		 		for (currentnum = burstProcess.begin(); currentnum != burstProcess.end(); currentnum++){
+		 			int temptime = smallestnum->burst;
+		 			if (currentnum->burst == temptime + timecalc)
+		 				currentnum->burst -= timecalc ;
+				 }
+			 }
+			else{
+			 	for (currentnum = Processid.begin(); currentnum != Processid.end(); currentnum++){
+		 			int temptime = smallestnum->burst;
+		 			if (currentnum->burst == temptime + timecalc)
+		 				currentnum->burst -= timecalc;
+				 }
+			 }
+		 	
+		 	if (smallestnum->burst == 0){  
+			   	smallestnum->waiting += total - smallestnum->arrive - originalburst[smallestnum->id];
+			   	smallestnum->turnaroundtime += total - smallestnum->arrive;
+		 		completed.push_back((*smallestnum)); 
+			 }
+		}	
+	}
+		sort(burstProcess.begin(), burstProcess.end(), sortPriority);
+		
+		for (deque<node>::iterator currentnum = burstProcess.begin(); currentnum != burstProcess.end(); currentnum++){
+			total += currentnum->burst;
+			currentnum->waiting += total - currentnum->arrive - originalburst[currentnum->id];
+			currentnum->turnaroundtime += total - currentnum->arrive;
+			waitingtime+=currentnum->waiting;
+			
+			completed.push_back((*currentnum));	
+		}
+		average = waitingtime/process;
+   ofstream myfile1;
+       if(!myfile1){
+       	myfile1.open("output.txt");
+       	
+	   }
+	   else {
+	   
+	     myfile1.open("output.txt",ios::app);
+	 }
+       myfile1<<"------------------------"<<endl;
+   
+   myfile1<<"\n Priority premptive:" << endl;
+  
+      for (int i = 0; i < completed.size(); i++){
+       myfile1<<"P" << completed[i].id <<":" << completed[i].waiting << endl;
+   }
+    myfile1<<"\n Average waiting time"<<average<<endl;
+   myfile1<<endl;
+   
+   myfile1.close();
+
+
+	
+	
+	
+	for (num = Processid.begin(); num != Processid.end(); num++){
+			num->waiting = 0;
+			num->turnaroundtime = 0;
+		}
+	
+	completed.clear();
+	
+}
+void CPU::shortestjobfirstpremptive(int process){
+	
+   ofstream myfile1;
+       if(!myfile1){
+       	myfile1.open("output.txt");
+       	
+	   }
+	   else {
+	   
+	     myfile1.open("output.txt",ios::app);
+	 }
+       myfile1<<"------------------------"<<endl;
+  
+   myfile1<<"\n shortestjob premptive is yet to be completed:" << endl;
+  
+    
+    
+   myfile1<<endl;
+   
+   myfile1.close();
+
+
+	
+}
+ 
+	 	
 
 
 
